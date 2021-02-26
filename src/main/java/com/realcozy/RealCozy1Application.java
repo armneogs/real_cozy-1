9f04ce91e7d415397cc80260624a0572b9e84fd9
Armneogs <33708099+armneogs@users.noreply.github.com>
2021-02-26 11:15:50 +0700

    Initial commit

---
+++ src/main/java/com/realcozy/RealCozy1Application.java
@@ -0,0 +1,13 @@
+package com.realcozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealCozy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealCozy1Application.class, args);
+	}
+
+}
